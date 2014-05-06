35f2180f3e30d44921a1702fca54fe9f3ea0e3c3
Rui Oliveiras <rui96pedro@hotmail.com>
2014-05-06 11:31:41 +0100

    Adiciona class low lvl

---
+++ src/model/activityLow/Basquetebol.java
@@ -0,0 +1,34 @@
+package model.activityLow;
+
+import model.activityHigh.Activity;
+import model.activityHigh.Collective;
+
+public class Basquetebol extends Collective {
+
+	@Override
+	public String getName() {
+		return "Basquetebol";
+	}
+
+	@Override
+	public int getRecordType() {
+		return 0;
+	}
+
+	@Override
+	public int compareRecord(Activity otherActivity) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getIntensidade() {
+		return 100;
+	}
+
+	@Override
+	public String getPointName() {
+		return "Pontos";
+	}
+
+}
